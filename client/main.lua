42522a240c5e9dc63d9d15d7ea38782f06c83e36
Torch ICO <tommyrpuk@gmail.com>
2024-03-22 17:14:11 +0000

    Something?

--- client/main.lua
+++ client/main.lua
@@ -1,11 +1,21 @@
-local data = ({
+local menu = ({
     title = 'Management Menu',
     description = 'Manage Employees',
     options = {
-
+        {
+            title = 'Button goes Brrr',
+            description = 'Idk this hurts my brain',
+            icon = 'circle',
+            onSelect = function()
+              print("It did something?")
+            end,
+            metadata = {
+              {label = 'Job', value = 'name?'},
+            },
+        },
     }
 })
-lib.registerContext(data)
+lib.registerContext(menu)
 
 RegisterNetEvent('ox_management:client:menu', function(players, groups)
 end)
