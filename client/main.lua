da257d7dc90d80faa7bb00eda8f4fd771f2791ee
Torch ICO <tommyrpuk@gmail.com>
2024-03-22 17:11:48 +0000

    I already hate my fucking life for doing this lol

--- client/main.lua
+++ client/main.lua
@@ -8,10 +8,4 @@ local data = ({
 lib.registerContext(data)
 
 RegisterNetEvent('ox_management:client:menu', function(players, groups)
-    for v in #groups do
-        for i in #players do
-        players = players[i]
-        
-        end
-    end
 end)
--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,11 @@
 RegisterNetEvent('ox_management:server:players', function(source)
 local players = player.GetPlayers()
 local groups = player.GetGroups()
-
+-- Fuck the logic for this is hard lmao
+local Players = {
+        player = players,
+        job = groups,
+}
 
 TriggerClientEvent('ox_management:client:menu', source, players, groups)
 end)
